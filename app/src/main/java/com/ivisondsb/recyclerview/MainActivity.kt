8ed71e2333c7a5478694831e2f39b457f8c488fe
Ivison <Ivisonk0@gmail.com>
2021-11-24 11:20:33 -0300

    create initRecyclerView

--- app/src/main/java/com/ivisondsb/recyclerview/MainActivity.kt
+++ app/src/main/java/com/ivisondsb/recyclerview/MainActivity.kt
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var liveAdapter: LiveAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+
+        this.liveAdapter = LiveAdapter()
+
     }
 }
